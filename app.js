cc971ca53c6106e8e769135c48616cfe21093f38
jenavieve-layosa <jlayosa@nevada.unr.edu>
2023-11-03 13:33:16 -0700

    first pass

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const planes = [
-    "Dominaria",
-    "Zendikar",
-    "Innistrad",
-    // Add more plane names as needed
+    "amy's home",
+    "antartic research base",
+    "aplan mortarium",
+    "bad wolf bay", 
+    "beseiged viking village", 
+    "bowie base one", 
+    "chaotic aether",
+    "city of the daleks",
+    "coal hill school", 
+    "dalek intensive care", 
+    "gardens of tranquil repose", 
+    "hotel of fears", 
+    "kerblam! warehouse", 
+    "lake silencio", 
+    "mondassian colony ship", 
+    "mutual epiphany", 
+    "new new york", 
+    "north pole research", 
+    "ood sphere", 
+    "pompeii", 
+    "prime miniter's cabinet room", 
+    "singing towers of darillium", 
+    "spectrox mines", 
+    "the doctor's childhood barn", 
+    "the doctor's tomb", 
+    "the drum, mining facility", 
+    "the lux foundation library", 
+    "the matrix of time", 
+    "the moonbase", 
+    "the pyramid of mars", 
+    "two streams facility", 
+    "unit headquarters",
+    // add more later 
+
 ];
 
 const planeImage = document.getElementById("plane-image");
@@ -11,7 +41,7 @@ const randomizeButton = document.getElementById("randomize-button");
 randomizeButton.addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * planes.length);
     const randomPlane = planes[randomIndex];
-    const imageUrl = `https://github.com/yourusername/mtg-plane-display/raw/main/images/${randomPlane}.jpg`;
+    const imageUrl = `https://github.com/jenavieve-layosa/mtg-plane-display/raw/main/images/${randomPlane}.jpg`;
 
     planeImage.src = imageUrl;
 });
